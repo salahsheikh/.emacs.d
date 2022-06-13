36b708d3dc87a38c605782c19538a9397bacfa60
Salah Sheikh <salah.h.sheikh@gmail.com>
2022-06-12 23:14:49 -0700

    Commit current workspace configuration

--- lisp/fpath-header-line.el
+++ lisp/fpath-header-line.el
@@ -1,19 +1,26 @@
-;; display nice file path in headerline
-(setq-default header-line-format
-    '(:eval
-    (concat
-        (if (and (not (string-suffix-p "*" (buffer-name))) (not (string-prefix-p "*" (buffer-name))))
-            (if (eq ml-selected-window (selected-window))
-                (propertize (get-buffer-title) 'face 'mode-line)
-            (propertize (get-buffer-title) 'face 'mode-line-inactive)) nil)
+(defun get-modified-buffer-background-property (active)
+  (defconst modified-background "yellow green")
+  (defconst unmodified-background "olive drab")
+  (defconst inactive-darken-percentage 20)
 
-        (if (buffer-modified-p)
-            " [UNSAVED]" nil))))
+  (if active      
+      (if (buffer-modified-p) modified-background unmodified-background)
+    (if (buffer-modified-p) (color-darken-name modified-background inactive-darken-percentage)
+      (color-darken-name unmodified-background (+ 15 inactive-darken-percentage)))))
+
+(defun get-window-number()
+  (window-parameter
+   (selected-window)
+   'ace-window-path))
 
 (defun get-buffer-title ()
   (if (buffer-file-name)
       (abbreviate-file-name (buffer-file-name))
     (abbreviate-file-name (buffer-name))))
+
+(defun get-space-padded-title (title)
+  (let ((window-number-width (if (featurep 'ace-window) (string-width (get-window-number)) 0)))
+    (concat "   " title (make-string (- (window-total-width) (string-width title) window-number-width) ? ))))
 
 (defvar ml-selected-window nil)
 
@@ -21,10 +28,21 @@
   (setq ml-selected-window (selected-window)))
 
 (defun ml-update-all ()
-  ;; (if (derived-mode-p 'prog-mode)
-  ;;     (setq left-fringe-width 0)
-  ;;   (setq left-fringe-width 8))
-  (setq right-fringe-width 0)
+  (when (buffer-file-name)
+    (setq
+     header-line-format
+     '(
+       :eval 
+       (concat
+        (when (featurep 'ace-window)
+          (propertize (get-window-number)
+                      'face '(:inherit mode-line :background "cadet blue")))
+        (if (eq ml-selected-window (selected-window))
+            (propertize (get-space-padded-title (get-buffer-title)) 'face
+                        `(:inherit mode-line :background ,(get-modified-buffer-background-property t)))
+          (propertize (get-space-padded-title (get-buffer-title)) 'face
+                      `(:inherit mode-line-inactive :background ,(get-modified-buffer-background-property nil))))))))
+
   (force-mode-line-update t))
 
 (add-hook 'post-command-hook 'ml-record-selected-window)
@@ -32,4 +50,3 @@
 (add-hook 'buffer-list-update-hook 'ml-update-all)
 
 (provide 'fpath-header-line)
-;; end of header line file display
